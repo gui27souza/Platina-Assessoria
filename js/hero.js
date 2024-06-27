c8fdb7c42917b15512a125bd8dfc7db55c14064a
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2024-06-27 09:36:47 -0300

    Fim nav e hero

--- js/hero.js
+++ js/hero.js
@@ -1,11 +1,13 @@
 const header_element = document.getElementById('header')
-const hero_element = document.getElementById('hero')
+const hero_element = document.getElementById('hero-banner')
 
 function heroHeight() {
 
+    if (window.innerWidth < 900) return
+
     let header_height = header_element.offsetHeight + 'px'
 
-    hero.style.height = `calc(100vh - ${header_height})`
+    hero_element.style.height = `calc(100vh - ${header_height})`
 }
 
 window.addEventListener('load', heroHeight())
